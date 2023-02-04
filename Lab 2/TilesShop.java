e9c999c85cdc23e29ed4f4de5f23d2fc2bf61caf
Arun-Thiyaagarajan <isronatking963.at@gmail.com>
2023-02-04 06:36:37 +0530

    Lab 2 Completion

--- Lab 2/TilesShop.java
+++ Lab 2/TilesShop.java
@@ -4,32 +4,82 @@ import java.util.Scanner;
 public class TilesShop {
     public static void main(String[] args) {
 
-        int n;
+        int n, choice;
         Scanner in = new Scanner(System.in);
 
         TilesInfo tile = new TilesInfo();
-        // BranchesInfo branch = new BranchesInfo();
+        BranchesInfo branch = new BranchesInfo();
 
-        System.out.print("\nHow many tiles info you gonna add? ");
-        n = in.nextInt();
+        System.out.print("\nMAHALAKSHMI CERRAMICS\n=========== =========");
+        System.out.print("\n1) Tiles Info");
+        System.out.print("\n2) Branches Info\n");
+        System.out.print("\nSelect which info you want to work on: ");
+        choice = in.nextInt();
 
-        tile.addTilesInfo(n);
-        tile.showTilesInfo(n);
+        if (choice == 1) {
+            System.out.print("\nHow many tiles info you gonna add? ");
+            n = in.nextInt();
+
+            tile.addTilesInfo(n);
+            tile.showTilesInfo(n);
+        }
+        else {
+            System.out.print("\nHow many branch info you gonna add? ");
+            n = in.nextInt();
+
+            branch.createBranch(n);
+            branch.showBranchInfo(n);
+        }
 
         in.close(); //Closing Scanner Class
     }
 }
 
-// class BranchesInfo {
+class BranchesInfo 
+{
+    int i;
+    String id, name;
+    ArrayList<String> branchID = new ArrayList<String>();
+    ArrayList<String> managerName = new ArrayList<String>();
     
-//     void createBranch() {
-        
-//     }
-// }
+    Scanner in = new Scanner(System.in);
+
+    void createBranch(int n) 
+    {
+        System.out.print("\nAdding Branch Info\n`````` ``````` ````");
+
+        for (i = 0; i < n; i++) {
+            System.out.printf("\nBranch Detail %d:\n====== ====== ==", i + 1);
+
+            System.out.print("\nBranch ID: ");
+            id = in.next();
+            branchID.add(id);
+
+            System.out.print("Manager Name: ");
+            name = in.next();
+            managerName.add(name);
+        }
+    }
+    
+    void showBranchInfo(int n) 
+    {
+        System.out.print("\n\nDisplay Branchs Info\n``````` ````` ````");
+    
+        for (i = 0; i < n; i++) 
+        {
+            System.out.printf("\nBranch Detail %d:\n==== ====== ==", i + 1);
+    
+            id = branchID.get(i);
+            name = managerName.get(i);
+    
+            System.out.printf("\nBranch ID: %s", id);
+            System.out.printf("\nManager Name: %s\n", name);
+        }
+    }
+}
 
 class TilesInfo 
 {
-
     int i;
     String id, design, size;
     ArrayList<String> tileID = new ArrayList<String>();
@@ -42,7 +92,7 @@ class TilesInfo
 
     void addTilesInfo(int n) 
     {
-        System.out.println("\nAdding Tiles Info\n`````` ````` ````");
+        System.out.print("\nAdding Tiles Info\n`````` ````` ````");
         
         for (i = 0; i < n; i++) 
         {
@@ -60,7 +110,7 @@ class TilesInfo
     
     void showTilesInfo(int n) 
     {
-        System.out.println("\n\nDisplay Tiles Info\n``````` ````` ````");
+        System.out.print("\n\nDisplay Tiles Info\n``````` ````` ````");
 
         for (i = 0; i < n; i++) 
         {
